d89c026188614e675935ecf7a3d615304c0f99c5
Waffy Mawanan <you@example.com>
2024-04-01 13:37:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showSideBar (addEventListener = 'click' ) {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+
+ }
+
+ function closeSideBar (addEventListener = 'click' ) {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+ }
